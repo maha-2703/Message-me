a65a1dffab4279fdf73da8272a0f6c1d079c684d
maha <maha@example.com>
2026-05-11 09:58:46 +0530

    message_me App

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,43 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "channels"
+
+const scroll_bottom = () => {
+
+  const container = $('#messages');
+
+  if(container.length > 0) {
+
+    container.scrollTop(container[0].scrollHeight);
+
+  }
+
+}
+
+document.addEventListener("turbo:load", function() {
+
+  $('.ui.dropdown').dropdown();
+
+  $('.message .close').on('click', function() {
+
+    $(this).closest('.message').transition('fade');
+
+  });
+
+  scroll_bottom();
+
+  $('#message_body').off('keydown').on('keydown', function(e) {
+
+    if(e.keyCode == 13 && !e.shiftKey) {
+
+      e.preventDefault();
+
+      $('#message-form').submit();
+
+      $(this).val('');
+
+    }
+
+  });
+
+});
